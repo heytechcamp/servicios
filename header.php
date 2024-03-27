07925e220aaffaa98776ca999d8e86c74150543b
heytechcamp <165224366+heytechcamp@users.noreply.github.com>
2024-03-27 17:52:46 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,10 @@
+<!-- header.php -->
+<header>
+    <a id="logo-button" href="index.php">
+        <img id="logo" src="logo isn fonfo.png" alt="Foto de perfil">
+    </a>
+    <a id="whatsapp-button" href="https://wa.link/231qc5" target="_blank">
+        <img src="whatsapp-logo-1.png" width="50px" alt="Logo de WhatsApp" >
+    </a>
+    <h1>Centro de Soporte Técnico Informático</h1>
+</header>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Servicios de Soporte Técnico Informático</title>
+<link rel="stylesheet" href="styles.css">
+
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    
+
+<section id="servicios">
+    <h2>Nuestros Servicios</h2>
+    <div class="servicio">
+        <h3>Soporte técnico informático</h3>
+        <p>Resolución de problemas de hardware. | Optimización del rendimiento del software. | Solución de errores y problemas de funcionamiento.</p>
+        <a href="https://wa.link/g6kbol" target="_blank" class="precio">Desde $200</a>
+
+
+    </div>
+    <div class="servicio">
+        <h3>Programación y desarrollo web</h3>
+        <p>Creación de sitios web desde cero. | Desarrollo de aplicaciones de escritorio personalizadas. | Desarrollo de aplicaciones web personalizadas.</p>
+        <a href="https://wa.link/cfh10z" target="_blank" class="precio">Desde $500</a>
+
+    </div>
+    <div class="servicio">
+        <h3>Administración de páginas de Facebook</h3>
+        <p>Creación y diseño de páginas. | Publicación de contenido relevante y atractivo. | Interacción con seguidores y respuesta a comentarios.</p>
+        <a href="https://wa.link/81ufl3" target="_blank" class="precio">Desde $200</a>
+
+    </div>
+    <div class="servicio">
+        <h3>Creación de perfiles en Google Maps</h3>
+        <p>Registro y configuración de perfiles comerciales. | Optimización de información para mejorar la visibilidad. | Gestión de reseñas y actualización de detalles comerciales.</p>
+        <a href="https://wa.link/gup9pl" target="_blank" class="precio">Desde $200</a>
+
+    </div>
+</section>
+
+<footer>
+    <p>© 2024 Centro de Soporte Técnico Informático. Todos los derechos reservados. <br>📌San Francisco de Campeche</p>
+</footer>
+</body>
+</html>
